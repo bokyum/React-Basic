e9e748022568f133d4dad57614037b6589a968fd
bokyum <95bogyeom@gmail.com>
2021-08-11 23:37:56 +0900

    setup

--- habit-tracker/src/App.jsx
+++ habit-tracker/src/App.jsx
@@ -13,17 +13,24 @@ class App extends Component {
   };
 
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      }
+      return item;
+    });
     this.setState({ habits });
   };
 
   handleDecrease = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+      return item;
+    });
     this.setState({ habits });
   };
 
@@ -41,7 +48,9 @@ class App extends Component {
   };
   handleReset = () => {
     const habits = this.state.habits.map((habit) => {
-      habit.count = 0;
+      if (habit.count !== 0) {
+        return { ...habit, count: 0 };
+      }
       return habit;
     });
     this.setState({ habits });
--- habit-tracker/src/components/HabitAddForm.jsx
+++ habit-tracker/src/components/HabitAddForm.jsx
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
+import React, { memo } from "react";
 
-class HabitAddForm extends Component {
-  inputRef = React.createRef();
-  formRef = React.createRef();
-  onSubmit = (event) => {
+const HabitAddForm = memo((props) => {
+  const inputRef = React.createRef();
+  const formRef = React.createRef();
+
+  const onSubmit = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
-    name && this.props.onAdd(name);
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
     //this.inputRef.current.value = ``;
-    this.formRef.current.reset();
+    formRef.current.reset();
   };
-  render() {
-    return (
-      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-        <input
-          ref={this.inputRef}
-          type="text"
-          className="add-input"
-          placeholder="..."
-        />
-        <button className="add-button">Add</button>
-      </form>
-    );
-  }
-}
+
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="..."
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
 
 export default HabitAddForm;
