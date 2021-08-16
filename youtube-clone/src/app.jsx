a66080276974d6a8e2cb6e63eecb2586b48c1bff
bokyum <95bogyeom@gmail.com>
2021-08-16 21:34:53 +0900

    search

--- youtube-clone/src/app.jsx
+++ youtube-clone/src/app.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import './app.css';
+import React, { useEffect, useState } from 'react';
+import SearchHeader from './components/search_header/search_header';
+import VideoList from './components/video_list/video_list';
+import styles from './app.module.css';
 
-function App() {
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
+
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
+  }, []);
   return (
-    <h1>Hello</h1>
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
